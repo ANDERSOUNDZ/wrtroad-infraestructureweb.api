50a022ead294a1c6132af8cc44eef580fb8567d9
Anderson mike Chanchay <andersonmikol@live.com>
2025-08-26 13:22:19 -0500

    updates

--- wrtroad-infraestructureweb.api/webapi/middlewares/BaseMiddleware.cs
+++ wrtroad-infraestructureweb.api/webapi/middlewares/BaseMiddleware.cs
@@ -8,7 +8,7 @@ namespace wrtroad_infraestructureweb.api.webapi.middlewares
     {
         public static IApplicationBuilder CustomMiddlewares(this IApplicationBuilder builder)
         {
-            //builder.UseMiddleware<ExceptionMiddleware>();
+            builder.UseMiddleware<ExceptionMiddleware>();
             //builder.UseMiddleware<CorsMiddleware>();
             //builder.UseMiddleware<AuthorizationMiddleware>();
             return builder;
